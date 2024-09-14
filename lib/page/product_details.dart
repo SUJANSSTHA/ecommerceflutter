0a2ccc9dfa88aceef1732754552e7b48e7a1d49a
SUJAN PRADHAN <123sujanpr@gmail.com>
2024-09-14 10:25:23 +0545

    product details UI completed

--- lib/page/product_details.dart
+++ lib/page/product_details.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerceflutter/widget/support_widget.dart';
 import 'package:flutter/material.dart';
 
 class ProductDetails extends StatefulWidget {
@@ -13,8 +14,7 @@ class _ProductDetailsState extends State<ProductDetails> {
     return Scaffold(
       backgroundColor: Color(0xFFfef5f1),
       body: Container(
-        // Icon(Icons.arrow_back),
-        padding: EdgeInsets.only(top: 50.0, left: 20.0),
+        padding: EdgeInsets.only(top: 50.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -37,14 +37,77 @@ class _ProductDetailsState extends State<ProductDetails> {
                 height: 400,
               )),
             ]),
-            Container(
-              decoration: BoxDecoration(color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft:Radius.circular(20),topRight: Radius.circular(20),
-                 )),
-              width: MediaQuery.of(context).size.width,
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+                margin: EdgeInsets.only(left: 10.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    )),
+                width: MediaQuery.of(context).size.width,
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Headphone",
+                            style: TextStyle(
+                              fontSize: 23.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "\$300",
+                            style: TextStyle(
+                              color: Color(0xFFfd6f3e),
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text(
+                        "Details",
+                        style: AppWidget.seamiboldTextFeildStyle(),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text(
+                        "This product is very good. It have a 1 year warrenty, this headphone is great for quality sound, even capturing very low audio levels.",
+                        style: AppWidget.lightTextFeildStyle(),
+                      ),
+                      SizedBox(height: 30.0),  // Add some space here
+                      Container(
+                        // padding: EdgeInsets.all(15.0),
+                        padding: EdgeInsets.symmetric(vertical: 15.0),
+                        decoration: BoxDecoration(
+                          color: Color(0xFFfd6f3e),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        width: MediaQuery.of(context).size.width,
+                        child: Center(
+                          child: Text(
+                            "Buy Now",
+                            style: TextStyle(
+                                color: Colors.white, fontSize: 20.0),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
-       
           ],
         ),
       ),
